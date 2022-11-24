73453e1782a730599c012b9e60139c03dc93136e
Roni Hämäläinen <roni@NLT.home>
2022-11-24 18:17:24 +0200

    Perform renaming

--- register-selftest-generator-runner/src/main.rs
+++ register-selftest-generator-runner/src/main.rs
@@ -1,13 +1,9 @@
-//mod old_register_tests;
-
-use register_selftest_generator;
+use register_selftest;
 
 fn main() {
     // TODO: enum TestType { Read, Reset, }
-    // TODO: excludes
 
-    //for (index, function) in old_register_tests::FUNCTIONS.iter().enumerate() {
-    for (index, function) in register_selftest_generator::FUNCTIONS.iter().enumerate() {
+    for (index, function) in register_selftest::FUNCTIONS.iter().enumerate() {
         let result = function();
         println!("{index}: {result}");
     }
