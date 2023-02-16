d1c2839b5271fb6b5521634a2b6575c21a2c7b02
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2023-02-16 10:34:14 +0200

    Comment out HPC specific code

--- register-selftest-generator-runner/src/main.rs
+++ register-selftest-generator-runner/src/main.rs
@@ -3,9 +3,11 @@ use register_selftest;
 fn main() {
     // TODO: enum TestType { Read, Reset, }
 
+    /*
     for (_index, test_case) in register_selftest::hpc::TEST_CASES.iter().enumerate() {
         println!("{}", test_case.uid);
     }
+    */
 
     for (_index, test_case) in register_selftest::TEST_CASES.iter().enumerate() {
         println!("{}", test_case.uid);
