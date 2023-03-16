a1d49e5873a531ce27409c05b8c961c2f3175063
Henri Lunnikivi <heggggxa@gmail.com>
2023-03-16 17:51:58 +0200

    Reduce code noise with iterators

--- register-test-generator/src/generate.rs
+++ register-test-generator/src/generate.rs
@@ -7,36 +7,38 @@ use std::collections::HashMap;
 
 /// Place test cases in modules.
 fn create_modules(
-    test_fns_by_periph: &HashMap<String, Vec<String>>,
-    test_defs_by_periph: &HashMap<String, Vec<String>>,
-) -> Vec<String> {
-    let mut modules = Vec::new();
-    for (periph_name, test_fns) in test_fns_by_periph {
-        let test_defs_combined: TokenStream = test_defs_by_periph
-            .get(periph_name)
-            .unwrap()
-            .join(",")
-            .parse()
-            .unwrap();
-        let len = test_fns.len();
-        let value: TokenStream = quote!( [#test_defs_combined] );
-        let test_def_arr = quote! {
-            pub static TEST_CASES: [TestCase; #len] = #value;
-        };
-        let mod_name: TokenStream = periph_name.to_lowercase().parse().unwrap();
-        let test_fns_catenated: TokenStream = test_fns.join("").parse().unwrap();
-        let module = quote! {
-            pub mod #mod_name {
-                use super::*;
+    test_fns_and_defs_by_periph: HashMap<String, Vec<(String, String)>>,
+) -> Vec<TokenStream> {
+    test_fns_and_defs_by_periph
+        .into_iter()
+        .map(|(periph_name, test_fns_and_defs)| {
+            // Separate test functions and definitions into two Vecs
+            let (test_fns, test_defs): (Vec<_>, Vec<_>) = test_fns_and_defs.into_iter().unzip();
 
-                #test_def_arr
+            // Create array for test definitions
+            let test_defs_combined: TokenStream = test_defs.join(", ").parse().unwrap();
+            let len = test_fns.len();
+            let value: TokenStream = quote!( [#test_defs_combined] );
+            let test_def_arr = quote! {
+                pub static TEST_CASES: [TestCase; #len] = #value;
+            };
 
-                #test_fns_catenated
+            // Create test functions
+            let mod_name: TokenStream = periph_name.to_lowercase().parse().unwrap();
+            let test_fns_catenated: TokenStream = test_fns.join("").parse().unwrap();
+
+            // Create module with both test definitions and test functions
+            quote! {
+                pub mod #mod_name {
+                    use super::*;
+
+                    #test_def_arr
+
+                    #test_fns_catenated
+                }
             }
-        };
-        modules.push(format!("{}", module));
-    }
-    modules
+        })
+        .collect_vec()
 }
 
 /// Collection of all test cases for this build.
@@ -185,8 +187,7 @@ impl<'r> RegTestGenerator<'r> {
 impl TestCases {
     /// Generate test cases for each register.
     pub fn from_registers(registers: &Registers) -> Result<TestCases, GenerateError> {
-        let mut test_fns_by_periph: HashMap<String, Vec<String>> = HashMap::new();
-        let mut test_defs_by_periph: HashMap<String, Vec<String>> = HashMap::new();
+        let mut test_fns_and_defs_by_periph = HashMap::new();
         for register in registers.iter() {
             let test_gen = RegTestGenerator::from_register(register);
 
@@ -196,24 +197,17 @@ impl TestCases {
             let test_def = test_gen.gen_test_def()?;
             let test_def_str = format!("{}", test_def);
 
-            test_fns_by_periph
+            test_fns_and_defs_by_periph
                 .entry(register.peripheral_name.clone())
                 .or_insert(vec![])
-                .push(test_fn_str);
-
-            test_defs_by_periph
-                .entry(register.peripheral_name.clone())
-                .or_insert(vec![])
-                .push(test_def_str);
+                .push((test_fn_str, test_def_str));
         }
 
-        let mod_strings = create_modules(&test_fns_by_periph, &test_defs_by_periph);
-        let mod_strings_combined = mod_strings.join("");
-
-        // Collect all test definitions into one big list
-        let test_defs = test_defs_by_periph
+        // Duplicate all test definitions into one big list
+        let test_defs = test_fns_and_defs_by_periph
             .values()
             .flatten()
+            .map(|(_func, def)| def)
             .cloned()
             .collect_vec();
         let test_case_count = test_defs.len();
@@ -222,8 +216,13 @@ impl TestCases {
             pub static TEST_CASES: [TestCase; #test_case_count] = [ #test_defs_combined ];
         };
 
+        let mod_strings = create_modules(test_fns_and_defs_by_periph)
+            .into_iter()
+            .map(|ts| ts.to_string())
+            .collect::<String>();
+
         Ok(TestCases {
-            test_cases: vec![mod_strings_combined, format!("{}", test_case_array)],
+            test_cases: vec![mod_strings, format!("{}", test_case_array)],
             test_case_count,
         })
     }
