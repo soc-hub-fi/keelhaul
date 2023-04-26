da162c22cf079065652f3ceaf34205115f1e4304
Henri Lunnikivi <heggggxa@gmail.com>
2023-04-26 11:00:38 +0300

    Remove unused function

--- register-test-generator/src/lib.rs
+++ register-test-generator/src/lib.rs
@@ -38,34 +38,6 @@ pub fn validate_path_existence(path_str: &str) -> PathBuf {
     }
 }
 
-/// Checks if path to a file exists, and create it if it does not exist.
-///
-/// # Panics
-///
-/// This function panics if path can not be accessed.
-/// This can happen if the operating system denies access to the path.
-pub fn get_or_create(path_str: &str) -> PathBuf {
-    match PathBuf::from(path_str).canonicalize() {
-        Ok(path) => match path.try_exists() {
-            Ok(exists) => {
-                assert!(exists, "Path {} does not exist.", path.display());
-                path
-            }
-            Err(error) => panic!("Path {} does not exist. {}", path.display(), error),
-        },
-        Err(error) => {
-            println!("Path {path_str} does not exist. {error}");
-            match File::create(path_str) {
-                Ok(_file) => {
-                    println!("Created new file to path {path_str}.");
-                    validate_path_existence(path_str)
-                }
-                Err(error) => panic!("Failed to create new file to path {path_str}. {error}"),
-            }
-        }
-    }
-}
-
 /// Try to extract path to excludes-file from environment variable.
 fn read_excludes_path_from_env() -> Option<PathBuf> {
     env::var("PATH_EXCLUDES")
