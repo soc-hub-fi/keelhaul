bdf8d06c0262beee580cd78dd1b8f27712c31731
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-03-14 12:09:47 +0200

    Re-organize VP

--- examples/headsail/headsail-selftest/renode.sh
+++ examples/headsail/headsail-selftest/renode.sh
@@ -4,4 +4,4 @@ set -x # echo on
 BASEDIR=$(dirname "$0")
 BIN=${BIN=$1}
 
-renode --console -e "set bin @$BIN; include @$BASEDIR/../../vp/headsail/run_sysctrl.resc"
+renode --console -e "set bin @$BIN; include @$BASEDIR/../../../vp/headsail/2_run_sysctrl.resc"
