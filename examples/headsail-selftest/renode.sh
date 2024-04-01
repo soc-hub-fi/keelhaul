499ca03727b73e6d1cf0a2196e2dff4042a0e492
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-04-01 13:13:22 +0300

    Submodule: update headsail-vp

--- examples/headsail-selftest/renode.sh
+++ examples/headsail-selftest/renode.sh
@@ -4,4 +4,4 @@ set -x # echo on
 BASEDIR=$(dirname "$0")
 BIN=${BIN=$1}
 
-renode --console -e "set bin @$BIN; include @$BASEDIR/../../vendor/headsail-vp/scripts/2_run_sysctrl.resc"
+renode --console -e "set bin @$BIN; include @$BASEDIR/../../vendor/headsail-vp/scripts/resc/2_run_sysctrl.resc"
